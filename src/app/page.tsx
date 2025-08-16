a73817c80d06af8be5d2e81c6202f48e2ede368f
Vladislav Adamets <adametsderschopfer@yandex.ru>
2025-08-17 01:52:51 +0200

    Добавлены размеры шрифтов

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
     return (
-        <p className="text-[16px] max-w-100 text-surface-quaternary">
-            Артак — агроэкономист, фермер, активист. Основал кооператив из 15 фермеров, чтобы защитить права крестьян.
-            Выступал против незаконного захвата земли, организовывал местные митинги.
-        </p>
+        <div className="container">
+            <h2 className={"max-w-88 text-xl"}>
+                Вы спрашиваете — мы отвечаем честно
+            </h2>
+        </div>
     );
 }
